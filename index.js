3cdcee107694d03df7e0cdc948679b6755ddee86
Manav Parmar <parmar.manav2000@gmail.com>
2020-04-18 19:39:59 +0530

    Basic Drumpad With Javascript

---
+++ index.js
@@ -0,0 +1,122 @@
+$(function () {
+    var a_note = document.getElementById('a');
+    $('.1').on('click touch', function () {
+        a_note.currentTime = 0;
+        a_note.volume = 1.0;
+        a_note.play();
+    });
+    var b_note = document.getElementById('b');
+    $('.2').on('click touch', function () {
+        b_note.currentTime = 0;
+        b_note.volume = 1.0;
+        b_note.play();
+    });
+    var c_note = document.getElementById('c');
+    $('.3').on('click touch', function () {
+        c_note.currentTime = 0;
+        c_note.volume = 1.0;
+        c_note.play();
+    });
+    var d_note = document.getElementById('d');
+    $('.4').on('click touch', function () {
+        d_note.currentTime = 0;
+        d_note.volume = 1.0;
+        d_note.play();
+    });
+    var e_note = document.getElementById('e');
+    $('.5').on('click touch', function () {
+        e_note.currentTime = 0;
+        e_note.volume = 1.0;
+        e_note.play();
+    });
+    var f_note = document.getElementById('f');
+    $('.6').on('click touch', function () {
+        f_note.currentTime = 0;
+        f_note.volume = 1.0;
+        f_note.play();
+    });
+    var g_note = document.getElementById('g');
+    $('.7').on('click touch', function () {
+        g_note.currentTime = 0;
+        g_note.volume = 1.0;
+        g_note.play();
+    });
+    var h_note = document.getElementById('h');
+    $('.8').on('click touch', function () {
+        h_note.currentTime = 0;
+        h_note.volume = 1.0;
+        h_note.play();
+    });
+    var i_note = document.getElementById('i');
+    $('.9').on('click touch', function () {
+        i_note.currentTime = 0;
+        i_note.volume = 1.0;
+        i_note.play();
+    });
+    var j_note = document.getElementById('j');
+    $('.10').on('click touch', function () {
+        j_note.currentTime = 0;
+        j_note.volume = 1.0;
+        j_note.play();
+    });
+    var k_note = document.getElementById('k');
+    $('.11').on('click touch', function () {
+        k_note.currentTime = 0;
+        k_note.volume = 1.0;
+        k_note.play();
+    });
+    var l_note = document.getElementById('l');
+    $('.12').on('click touch', function () {
+        l_note.currentTime = 0;
+        l_note.volume = 1.0;
+        l_note.play();
+    });
+    var m_note = document.getElementById('m');
+    $('.13').on('click touch', function () {
+        m_note.currentTime = 0;
+        m_note.volume = 1.0;
+        m_note.play();
+    });
+    var n_note = document.getElementById('n');
+    $('.14').on('click touch', function () {
+        n_note.currentTime = 0;
+        n_note.volume = 1.0;
+        n_note.play();
+    });
+    var o_note = document.getElementById('o');
+    $('.15').on('click touch', function () {
+        o_note.currentTime = 0;
+        o_note.volume = 1.0;
+        o_note.play();
+    });
+    var p_note = document.getElementById('p');
+    $('.16').on('click touch', function () {
+        p_note.currentTime = 0;
+        p_note.volume = 1.0;
+        p_note.play();
+    });
+    var q_note = document.getElementById('q');
+    $('.17').on('click touch', function () {
+        q_note.currentTime = 0;
+        q_note.volume = 1.0;
+        q_note.play();
+    });
+    var r_note = document.getElementById('r');
+    $('.18').on('click touch', function () {
+        r_note.currentTime = 0;
+        r_note.volume = 1.0;
+        r_note.play();
+    });
+    var s_note = document.getElementById('s');
+    $('.19').on('click touch', function () {
+        s_note.currentTime = 0;
+        s_note.volume = 1.0;
+        s_note.play();
+    });
+    var t_note = document.getElementById('t');
+    $('.20').on('click touch', function () {
+        t_note.currentTime = 0;
+        t_note.volume = 1.0;
+        t_note.play();
+    });
+}());
